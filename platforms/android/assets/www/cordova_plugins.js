7cf3e1ca946f7110f31974ccb56fcc3160ae2900
Amr Sobhy <amrsobhy@gmail.com>
2016-01-29 21:39:09 +0000

    first-commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/pushbots-cordova-plugin/www/pushbots.js",
+        "id": "pushbots-cordova-plugin.PushbotsPlugin",
+        "pluginId": "pushbots-cordova-plugin",
+        "clobbers": [
+            "PushbotsPlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "pushbots-cordova-plugin": "1.3.0",
+    "cordova-plugin-android-support-v4": "21.0.1",
+    "cordova-plugin-whitelist": "1.2.1"
+}
+// BOTTOM OF METADATA
+});
